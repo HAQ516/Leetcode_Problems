cc359c456ea57dbea1f319f17b10eeb8d0320a82
Hafiz Abdul Qadir <abdulqadirhafiz9@gmail.com>
2026-08-22 00:19:04 +0500

    Time: 0 ms (100%), Space: 8.6 MB (51.22%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+        int l=1,r=x,ans=0;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if((long)mid*mid==x){
+                return mid;
+            }else if((long)mid*mid<x){
+                ans=mid;
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return ans;
+    }
+};
